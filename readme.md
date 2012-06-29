633f634530fd1b4af20342ef18a4dd12b1fd1643
Steve Harris <steve3001@gmail.com>
2012-06-30 00:36:16 +0100

    Commands added! Ctrl-shift-r sends selection (or line if nothing selected) to stata Ctrl-command-r sends the whole file

--- readme.md
+++ readme.md
@@ -6,7 +6,16 @@ There are a few modifications
 - compound quotes are supported
 - digits are highlighted
 
-There are a few problems
+## Commands
+
+There are also two commands to send code to stata from sublime text. Both work by creating a temporary file in your current working directory (otherwise references within the code that depend on relative paths would fail).
+
+- do file (bound to ctrl-command-r)
+- do selected text (bound to ctrl-shift-r) - and if there is no text selected then it will send the current line
+
+## Known issues
+
+- the commands are mac specific (they depend on osascript)
 - double forward slash doesn't work as a comment marker at the beginning of a line
 
 Hope it might be useful.
--- text_2_stata.py
+++ text_2_stata.py
@@ -2,8 +2,8 @@ import os
 import sublime_plugin
 import sublime
 
-class file_2_stataCommand(sublime_plugin.TextCommand):
-	""" copy buffer to temp file and send to stata """
+class text_2_stataCommand(sublime_plugin.TextCommand):
+	""" copy SELECTION to temp file and send to stata """
 
 	def run(self, edit):
 		#  get path to current directory
@@ -11,9 +11,14 @@ class file_2_stataCommand(sublime_plugin.TextCommand):
 		filepath = os.path.dirname(filename)
 
 		#  grab the buffer
-		region = sublime.Region(0, self.view.size())
-		# print "%r" % region
-		all_text =  self.view.substr(region)
+		#  if nothing selected then send the line
+		all_text = ""
+		sels = self.view.sel()
+		for sel in sels:
+			all_text = all_text + self.view.substr(sel)
+		if len(all_text) == 0:
+			all_text = self.view.substr(self.view.line(sel)) 
+		all_text = all_text + "\n"
 
 		#  write the buffer to file in pwd
 		dofile_path = os.path.join(filepath, 'sublime2stata.do')
@@ -21,9 +26,6 @@ class file_2_stataCommand(sublime_plugin.TextCommand):
 		this_file = open(dofile_path,'w')
 		this_file.write(all_text)
 		this_file.close()
-
-		# FIXME: 2012-06-29 - temp file while debugginf
-		# dofile_path = "~/data/aki/vcode/sublime2stata.do"
 
 		#  now call the file using osascript
 		cmd = """osascript<< END
