b87da1a497e7986872806c617ca203424f105b11
Steve Harris <steve3001@gmail.com>
2012-06-29 17:12:56 +0100

    Basic functioning version of command to send current do file to stata
    Notes / todos
    - no error checking (will try and run even if file not stata code)
    - sends whole file not the current selection

--- file_2_stata.py
+++ file_2_stata.py
@@ -1,13 +1,36 @@
-import os, sys
-import subprocess
-import sublime_plugin 
+import os
+import sublime_plugin
+import sublime
 
-class file_2_stataCommand(sublime_plugin.WindowCommand):  
-    def run(self):  
-        filename = self.window.active_view().file_name()
+class file_2_stataCommand(sublime_plugin.TextCommand):
+	""" copy buffer to temp file and send to stata """
 
-        proc_env = os.environ.copy()
-        for k, v in proc_env.iteritems():
-            proc_env[k] = os.path.expandvars(v).encode(sys.getfilesystemencoding())
+	def run(self, edit):
+		#  get path to current directory
+		filename = self.view.file_name()
+		filepath = os.path.dirname(filename)
 
-        subprocess.call(['open', '-a', 'Marked', filename], env=proc_env)
+		#  grab the buffer
+		region = sublime.Region(0, self.view.size())
+		# print "%r" % region
+		all_text =  self.view.substr(region)
+
+		#  write the buffer to file in pwd
+		dofile_path = os.path.join(filepath, 'sublime2stata.do')
+		print "%r" % dofile_path
+		this_file = open(dofile_path,'w')
+		this_file.write(all_text)
+		this_file.close()
+
+		# FIXME: 2012-06-29 - temp file while debugginf
+		dofile_path = "~/data/aki/vcode/sublime2stata.do"
+
+		#  now call the file using osascript
+		cmd = """osascript<< END
+		 tell application "stata" 
+			activate
+			open POSIX file "%s"
+		 end tell
+		 END""" % dofile_path
+		print cmd
+		os.system(cmd)
