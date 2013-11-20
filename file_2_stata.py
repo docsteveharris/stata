40a9454b57babb3aa5defd3cd023f1819772cd97
Steve Harris <me@steveharris.me>
2013-11-20 17:40:59 +0000

    updated: now works with Stata 12
    Note you *must* go to Preferences>Do-file editor>Advanced and uncheck the box which says "Edit do-files opened from the Finder in Do-file Editor"
    
    Signed-off-by: Steve Harris <me@steveharris.me>

--- file_2_stata.py
+++ file_2_stata.py
@@ -3,34 +3,36 @@ import sublime_plugin
 import sublime
 
 class file_2_stataCommand(sublime_plugin.TextCommand):
-	""" copy buffer to temp file and send to stata """
+    """ copy buffer to temp file and send to stata """
 
-	def run(self, edit):
-		#  get path to current directory
-		filename = self.view.file_name()
-		filepath = os.path.dirname(filename)
+    def run(self, edit):
+        #  get path to current directory
+        filename = self.view.file_name()
+        # print(filename)
+        # print("So far, so good")
+        filepath = os.path.dirname(filename)
 
-		#  grab the buffer
-		region = sublime.Region(0, self.view.size())
-		# print "%r" % region
-		all_text =  self.view.substr(region)
+        #  grab the buffer
+        region = sublime.Region(0, self.view.size())
+        # print "%r" % region
+        all_text =  self.view.substr(region)
+        # Now prepend the file path
+        # so the first thing stata does is to change to the working directory
+        all_text = '''qui cd "''' + filepath + '''"\n ''' + all_text
+        print(all_text)
 
-		#  write the buffer to file in pwd
-		dofile_path = os.path.join(filepath, 'sublime2stata.do')
-		print "%r" % dofile_path
-		this_file = open(dofile_path,'w')
-		this_file.write(all_text)
-		this_file.close()
 
-		# FIXME: 2012-06-29 - temp file while debugginf
-		# dofile_path = "~/data/aki/vcode/sublime2stata.do"
+        #  write the buffer to file in pwd
+        dofile_path = os.path.join(filepath, 'sublime2stata.do')
+        # print "%r" % dofile_path
+        this_file = open(dofile_path,'w')
+        this_file.write(all_text)
+        this_file.close()
 
-		#  now call the file using osascript
-		cmd = """osascript<< END
-		 tell application "stata" 
-			activate
-			open POSIX file "%s"
-		 end tell
-		 END""" % dofile_path
-		print cmd
-		os.system(cmd)
+        cmd = """osascript<< END
+         tell application "Finder"
+          open POSIX file "%s"
+         end tell
+        END""" % dofile_path
+        os.system(cmd)
+
--- text_2_stata.py
+++ text_2_stata.py
@@ -3,36 +3,36 @@ import sublime_plugin
 import sublime
 
 class text_2_stataCommand(sublime_plugin.TextCommand):
-	""" copy SELECTION to temp file and send to stata """
+    """ copy SELECTION to temp file and send to stata """
 
-	def run(self, edit):
-		#  get path to current directory
-		filename = self.view.file_name()
-		filepath = os.path.dirname(filename)
+    def run(self, edit):
+        #  get path to current directory
+        filename = self.view.file_name()
+        filepath = os.path.dirname(filename)
+        print(filepath)
 
-		#  grab the buffer
-		#  if nothing selected then send the line
-		all_text = ""
-		sels = self.view.sel()
-		for sel in sels:
-			all_text = all_text + self.view.substr(sel)
-		if len(all_text) == 0:
-			all_text = self.view.substr(self.view.line(sel)) 
-		all_text = all_text + "\n"
+        #  grab the buffer
+        #  if nothing selected then send the line
+        all_text = ""
+        sels = self.view.sel()
+        for sel in sels:
+            all_text = all_text + self.view.substr(sel)
+        if len(all_text) == 0:
+            all_text = self.view.substr(self.view.line(sel)) 
+        all_text = all_text + "\n"
 
-		#  write the buffer to file in pwd
-		dofile_path = os.path.join(filepath, 'sublime2stata.do')
-		print "%r" % dofile_path
-		this_file = open(dofile_path,'w')
-		this_file.write(all_text)
-		this_file.close()
+        #  write the buffer to file in pwd
+        dofile_path = os.path.join(filepath, 'sublime2stata.do')
+        # print "%r" % dofile_path
+        this_file = open(dofile_path,'w')
+        this_file.write(all_text)
+        this_file.close()
 
-		#  now call the file using osascript
-		cmd = """osascript<< END
-		 tell application "stata" 
-			activate
-			open POSIX file "%s"
-		 end tell
-		 END""" % dofile_path
-		print cmd
-		os.system(cmd)
+        cmd = """osascript<< END
+         tell application "Finder" 
+          open POSIX file "%s"
+         end tell
+        END""" % dofile_path
+        os.system(cmd)
+
+
